dff821b13137dd5341f62f421294d51104cbbc1a
david-ale <125339205+david-ale@users.noreply.github.com>
2025-04-30 18:37:59 -0500

    Fixing routes

--- frontend/src/app/features/auth/AuthButton.tsx
+++ frontend/src/app/features/auth/AuthButton.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function AuthButton() {
   return (
     <Link
-      href="/api/auth/login"
+      href="/auth/login"
       className="bg-white border border-black text-black px-6 py-3 rounded-xl block text-center w-full font-medium transform transition-transform duration-300 hover:scale-105 shadow-lg"
     >
       Iniciar sesión
--- frontend/src/app/features/auth/LogoutButton.tsx
+++ frontend/src/app/features/auth/LogoutButton.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function LogoutButton() {
   return (
     <Link
-      href="/api/auth/logout"
+      href="/auth/logout"
       className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg text-sm"
     >
       Cerrar sesión
