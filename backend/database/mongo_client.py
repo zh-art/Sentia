087c31d2eff9f8b66df9841838ba3743aeb777e4
david-ale <125339205+david-ale@users.noreply.github.com>
2025-05-28 17:40:09 -0500

    Config database atlas

--- backend/database/mongo_client.py
+++ backend/database/mongo_client.py
@@ -1,7 +1,18 @@
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["sentia_db"]
+# Cargar variables de entorno desde un archivo .env
+load_dotenv()
+
+# Obtener la URI de conexión desde la variable de entorno
+MONGO_URI = os.getenv("MONGO_URI")
+
+# Crear el cliente con la URI de Atlas
+client = MongoClient(MONGO_URI)
+
+# Seleccionar la base de datos y las colecciones
+db = client["SentiaDb"]  # Asegúrate de que este sea el nombre de tu base en Atlas
 chat_collection = db["chats"]
 user_collection = db["users"]
 report_collection = db["reports"]
